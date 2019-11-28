f69fadc627c2db0c859d28d9005ff6c54b54225e
kombud <kombud@localhost.localdomain>
2019-11-28 15:02:41 +0100

    Dodano funkcje do obslugi kontrolera trybu gry

--- src/application/ModeController.java
+++ src/application/ModeController.java
@@ -1,9 +1,48 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
 public class ModeController {
 
+	@FXML
+	private Button startButton;
+
+	@FXML
+	private TextField addressIPTextField;
+
+	@FXML
+	private TextField portTextField;
+
+	@FXML
+	private ComboBox modeComboBox;
+
+	public ModeController() {
+	///	
+
+	}
 	
-	public ModeController()
-	{}
+	// inicjalizacja elementow FXML
+	///////////////////////////////////////////////////////
+	@FXML
+    public void initialize() {
+		modeComboBox.getItems().addAll("Serwer", "Klient");
+    }
+
+	// obsluga handlera przycisku zacznij nowa gre
+	//////////////////////////////////////////////////
+	public void onPortTextFieldActionClick(ActionEvent event) {
+
+	}
 	
+	// obsluga handlera combo do wyboru trybu
+	//////////////////////////////////////////////////
+	public void onModeComboBoxActionClick(ActionEvent event) {
+
+	}
+
 }
